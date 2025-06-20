7055c1b0e974a6aaa039782bc3f8a21f93181030
kilicCeren <kiliccrn07@gmail.com>
2025-06-20 12:46:02 +0300

    v22

---
+++ src/test/java/testDatalari/TestDataDummyExample.java
@@ -0,0 +1,82 @@
+package testDatalari;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestDataDummyExample {
+
+    public static int basariliSorguStatusCode =200;
+    public static String contentType ="application/json";
+    // public static String headerConnection ="keep-alive";
+
+
+    /*
+    {
+                "status": "success",
+                "data": {
+                    "id": 3,
+                    "employee_name": "Ashton Cox",
+                    "employee_salary": 86000,
+                    "employee_age": 66,
+                    "profile_image": ""
+                    },
+                "message": "Successfully! Record has been fetched."
+            }
+     */
+
+    public static JSONObject jsonBodyOlustur(int id,
+                                             String employee_name,
+                                             int employee_salary,
+                                             int  employee_age,
+                                             String profile_image){
+
+        JSONObject responseBody =new JSONObject();
+        JSONObject dataBody =new JSONObject();
+
+        dataBody.put("id",id);
+        dataBody.put("employee_name",employee_name);
+        dataBody.put("employee_salary",employee_salary);
+        dataBody.put("employee_age",employee_age);
+        dataBody.put("profile_image",profile_image);
+
+        responseBody.put("status","success");
+        responseBody.put("data",dataBody);
+        responseBody.put("message","Successfully! Record has been fetched.");
+
+    return responseBody;
+    }
+
+    /*
+     Response Body
+        {
+            "status": "success",
+            "data": {
+                "id": 3,
+                "employee_name": "Ashton Cox",
+                "employee_salary": 86000,
+                "employee_age": 66,
+                "profile_image": ""
+                },
+        "message": "Successfully! Record has been fetched."
+        }
+     */
+    public static Map<String,Object> mapBodyOlustur(){
+
+        Map<String,Object> bodyMap =new HashMap<>();
+        Map<String,Object> dataMap =new HashMap<>();
+
+        dataMap.put("id",3);
+        dataMap.put("employee_name","Ashton Cox");
+        dataMap.put("employee_salary",86000.0);
+        dataMap.put("employee_age",66.0);
+        dataMap.put("profile_image","");
+
+        bodyMap.put("status","success");
+        bodyMap.put("data",dataMap);
+        bodyMap.put("message","Successfully! Record has been fetched.");
+
+        return  bodyMap;
+    }
+}
